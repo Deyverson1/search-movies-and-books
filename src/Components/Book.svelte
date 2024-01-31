66e91823748166a48915ffb149247ab46f2862d7
Deyverson1 <deyversongp@gmail.com>
2024-01-31 11:44:00 -0500

    Search for available books, to do: styles and responsive page.

---
+++ src/Components/Book.svelte
@@ -0,0 +1,56 @@
+<script>
+    export let title,publishedDate,description,infoLink, imageLinks = {}, authors = {};
+    
+</script>
+<section>
+    <article>
+        <section>
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </section>
+        <div>
+            <h4>{authors}</h4>
+            <h5>{publishedDate}</h5>
+            <img src={imageLinks.thumbnail} alt={title}>
+            <a target="_blank" href={infoLink}>Visit</a>
+        </div>
+    </article>
+</section>
+
+<style>
+    section{
+        display: flex;
+        flex-direction: column;
+        padding: .5rem;
+        box-sizing: border-box;
+        align-items: center;
+    }
+    h3{
+        
+        text-align: center;
+        margin: 0;
+    }
+    p{
+        max-width: 500px;
+
+    }
+    article{
+        display: flex;
+        padding: 1rem 3rem;
+        align-items: center;
+        gap: 10px;
+        border-radius: 10px;
+        height: auto;
+        border: 1px solid #ccc;
+        background-color: #ccc;
+        margin: 1rem;
+    }
+    div{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    img{
+       max-width: 192px;
+    }
+</style>
